e1c8ed4e908bb4274f80fe6393a838c6f1d2f844
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-18 13:57:35 +0100

    linked up to db - now retrieves data

--- App.js
+++ App.js
@@ -7,8 +7,7 @@ import {
 	TextInput,
 	Button,
 	Image,
-	ScrollView
-
+	ScrollView,
 } from "react-native";
 import NavBar from "./Components/NavBar";
 import AppButton from "./Components/Button";
@@ -20,14 +19,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import NewButton from "./Components/NewButton";
 import CatRating from "./Components/Rating";
-import { CLOUDINARY_PRESET, CLOUDINARY_CLOUD } from "@env";
-
-
-
+import { CLOUDINARY_PRESET, CLOUDINARY_CLOUD, DB_URL } from "@env";
 
 import * as ImagePicker from "expo-image-picker";
-
-
 
 function HomeScreen({ navigation }) {
 	const [location, setLocation] = useState(null);
@@ -38,15 +32,39 @@ function HomeScreen({ navigation }) {
 	// 	markerLat: 53.437957,
 	// 	markerLong: -2.213814,
 	// });
-	const [catInfo, setCatInfo] = useState({
-		id: 1,
-		lat: 53.437957,
-		long: -2.213814,
-		friendliness: 3,
-		cuteness: 5,
-		comments: "Great cat!",
-		photo: "./cat.jpeg"
-	});
+	const [catInfo, setCatInfo] = useState([]);
+	const [loading, setLoading] = useState(true);
+	// 	{
+	// 	id: 1,
+	// 	lat: 53.437957,
+	// 	long: -2.213814,
+	// 	friendliness: 3,
+	// 	cuteness: 5,
+	// 	comments: "Great cat!",
+	// 	photo: "./cat.jpeg",
+	// }
+
+	const getCats = async () => {
+		try {
+			const response = await fetch(
+				`${DB_URL}`
+			);
+			const json = await response.json();
+			setCatInfo(json.payload);
+			console.log("json",json);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getCats();
+	}, []);
+	
+
+
 
 	useEffect(() => {
 		(async () => {
@@ -77,7 +95,11 @@ function HomeScreen({ navigation }) {
 	// 	console.log("no location data");
 	// }
 
-
+		if (catInfo) {
+			console.log("catInfo", catInfo);
+		} else {
+			console.log("no cat data");
+		}
 
 	return (
 		<View style={styles.container}>
@@ -85,12 +107,13 @@ function HomeScreen({ navigation }) {
 			<Text style={{ color: "#993955", fontSize: "20%", padding: "5%" }}>
 				Find and rate cats in your area!
 			</Text>
+			
 			<ViewMap
 				lat={lat}
 				long={long}
 				markerLat={catInfo.lat}
 				markerLong={catInfo.long}
-				photo ="./cat.jpeg"
+				photo="./cat.jpeg"
 				friendliness={catInfo.friendliness}
 				cuteness={catInfo.cuteness}
 				comments={catInfo.comments}
@@ -106,95 +129,95 @@ function HomeScreen({ navigation }) {
 
 function AddCatScreen() {
 	const [comment, setComment] = useState("");
-	const [friendliness, setFriendliness] = useState(0)
+	const [friendliness, setFriendliness] = useState(0);
 	const [cuteness, setCuteness] = useState(0);
 	const [image, setImage] = useState(null);
 	const [photo, setPhoto] = useState(null);
 	const [base64, setBase64] = useState(null);
 	const [type, setType] = useState(null);
 
-		const pickImage = async () => {
-			// No permissions request is necessary for launching the image library
-			let result = await ImagePicker.launchImageLibraryAsync({
-				mediaTypes: ImagePicker.MediaTypeOptions.All,
-				allowsEditing: true,
-				aspect: [4, 3],
-				quality: 0.2,
-				base64: true,
-			});
+	const pickImage = async () => {
+		// No permissions request is necessary for launching the image library
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 0.2,
+			base64: true,
+		});
 
-			console.log(result);
+		// console.log(result);
 
-			if (!result.cancelled) {
-				setImage(result.uri);
-				setBase64(result.base64);
-				setType(result.type);
-			}
-		};
+		if (!result.cancelled) {
+			setImage(result.uri);
+			setBase64(result.base64);
+			setType(result.type);
+		}
+	};
 
-		console.log(image)
+	// console.log(image);
 
-
-
-		const cloudinaryUpload = async () => {
-			console.log("submit cat pressed");
-			console.log(photo);
-			const data = new FormData();
-			data.append("file", `data:${type};base64,${base64}`);
-			data.append("upload_preset", `${CLOUDINARY_PRESET}`);
-			data.append("cloud_name", `${CLOUDINARY_CLOUD}`);
-			console.log(data);
-			await fetch(`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD}/image/upload`, {
+	const cloudinaryUpload = async () => {
+		console.log("submit cat pressed");
+		console.log(photo);
+		const data = new FormData();
+		data.append("file", `data:${type};base64,${base64}`);
+		data.append("upload_preset", `${CLOUDINARY_PRESET}`);
+		data.append("cloud_name", `${CLOUDINARY_CLOUD}`);
+		console.log(data);
+		await fetch(
+			`https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD}/image/upload`,
+			{
 				method: "post",
 				body: data,
-			})
-				.then((res) => res.json())
-				.then((data) => {
-					console.log("147", data)
-					setPhoto(data.secure_url);
-					console.log("147, photo", photo);
-				})
-				.catch((err) => {
-					console.log("An Error Occured While Uploading");
-				});
-		};
-
-			if (photo) {
-				console.log("photo", photo);
-			} else {
-				console.log("no photo data");
 			}
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log("147", data);
+				setPhoto(data.secure_url);
+				console.log("147, photo", photo);
+			})
+			.catch((err) => {
+				console.log("An Error Occured While Uploading");
+			});
+	};
+
 
 
 	return (
-
-			<View style={styles.container}>
-				<NavBar style={styles.navBar} />
-				<Text style={{ color: "#993955", fontSize: "20%", padding: "5%" }}>
-					Rate the cat that you have spotted:
-				</Text>
-				<View style={styles.wrapper}>
-					<Text style={styles.options}>Friendliness:</Text>
-					<CatRating rating={setFriendliness}></CatRating>
-				</View>
-				<View style={styles.wrapper}>
-					<Text style={styles.options}>Cuteness:</Text>
-					<CatRating rating={setCuteness}></CatRating>
-				</View>
-				<Text style={styles.comment}>Comments (optional):</Text>
-				<TextInput
-					style={styles.input}
-					placeholder="Type here"
-					value={comment}
-					onChangeText={setComment}
-				></TextInput>
-				<NewButton text={"Upload Photo"} onPress={pickImage}></NewButton>
-				{image && (
-					<Image source={{ uri: image }} style={{ width: 100, height: 100, marginTop: "2%" }} />
-				)}
-				<NewButton text={"Submit Cat"} onPress={ ()=>cloudinaryUpload(image)}></NewButton>
+		<View style={styles.container}>
+			<NavBar style={styles.navBar} />
+			<Text style={{ color: "#993955", fontSize: "20%", padding: "5%" }}>
+				Rate the cat that you have spotted:
+			</Text>
+			<View style={styles.wrapper}>
+				<Text style={styles.options}>Friendliness:</Text>
+				<CatRating rating={setFriendliness}></CatRating>
 			</View>
-
+			<View style={styles.wrapper}>
+				<Text style={styles.options}>Cuteness:</Text>
+				<CatRating rating={setCuteness}></CatRating>
+			</View>
+			<Text style={styles.comment}>Comments (optional):</Text>
+			<TextInput
+				style={styles.input}
+				placeholder="Type here"
+				value={comment}
+				onChangeText={setComment}
+			></TextInput>
+			<NewButton text={"Upload Photo"} onPress={pickImage}></NewButton>
+			{image && (
+				<Image
+					source={{ uri: image }}
+					style={{ width: 100, height: 100, marginTop: "2%" }}
+				/>
+			)}
+			<NewButton
+				text={"Submit Cat"}
+				onPress={() => cloudinaryUpload(image)}
+			></NewButton>
+		</View>
 	);
 }
 
